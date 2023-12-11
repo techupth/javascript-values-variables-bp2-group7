184fc937116e3a2ee36f65c04903a9254b43a5e7
neung <neunghemdaeng.gmail.com>
2023-12-11 16:39:24 +0700

    Assignment: Values Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter"
+let lastName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const restaurantName = "Chef's Table by Chef James"
+const netPrice = 15000
+const allOfRestaurant = 1500
+
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”
+netPrice = 5000
+allOfRestaurant = 2500
+
+
